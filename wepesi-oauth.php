ef61291a184bdeca69c2d6e6726ca36caefe216c
David Njuguna <dnjuguna@gmail.com>
2014-08-26 10:45:14 +0300

    syntax eror

--- wepesi-oauth.php
+++ wepesi-oauth.php
@@ -1,17 +1,18 @@
 <?php
-/ * Plugin Name: Wepesi Ouath
-  * Description: Oauth
-  */
 
-function wepesi_oauth_endpoint($vars = ''){
+/*
+Plugin Name: Wepesi Ouath
+Description: Oauth
+*/
+
+add_action('init', 'wepesi_oauth_init');
+function wepesi_oauth_init($vars = ''){
 	add_rewrite_endpoint( 'oauth', EP_ROOT );
 	return $vars;
 }
 
-function wepesi_oauth_redirect($vars = ''){
-	print json_encode($vars)
+add_filter('request', 'wepesi_oauth_request');
+function wepesi_oauth_request($vars = ''){
+	print json_encode($vars);
 	return $vars;
 }
-
-add_action('init', 'wepesi_oauth_endpoint');
-add_filter('request', 'wepesi_oauth_redirect');
